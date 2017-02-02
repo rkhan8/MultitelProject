f10fa8a19b651bceeaab52511c306594a1836238
ange martial <ange martial>
2017-02-02 15:03:46 -0500

    test push

--- my-app/index.js
+++ my-app/index.js
@@ -7,6 +7,7 @@ var io = require('socket.io')(http);
 //initialize the repository. Start the localhost server with index.html file
 app.use(express.static(__dirname +'/frontend/'));
 
+
 //listening the node server on port 3000
 http.listen(3000, function () {
     console.log('listening on *:3000');
