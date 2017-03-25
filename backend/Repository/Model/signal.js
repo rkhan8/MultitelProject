e045117b00c130bb3a132d8b6a2a9fe782ae8769
ange martial <ange martial>
2017-03-25 04:18:25 -0400

    Repository plus ou moins fonctionnel reste a ajouter les test unitaires sur les nouvelles classes et mocker le service de persistance dans signalService

---
+++ backend/Repository/Model/signal.js
@@ -0,0 +1,29 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('signals', {
+    idN: {
+      type: DataTypes.STRING(10),
+      allowNull: false,
+      primaryKey: true
+    },
+    Category: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    MinVal: {
+      type: "DOUBLE",
+      allowNull: false
+    },
+    MaxVal: {
+      type: "DOUBLE",
+      allowNull: false
+    },
+    Unity: {
+      type: DataTypes.STRING(10),
+      allowNull: true
+    }
+  }, {
+    tableName: 'signals'
+  });
+};
---
+++ backend/Repository/Model/signalValue.js
@@ -0,0 +1,24 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('signalvalue', {
+    idN: {
+      type: DataTypes.STRING(10),
+      allowNull: false,
+      references: {
+        model: 'signals',
+        key: 'idN'
+      }
+    },
+    ValueRec: {
+      type: "DOUBLE",
+      allowNull: false
+    },
+    DateRec: {
+      type: DataTypes.DATE,
+      allowNull: true
+    }
+  }, {
+    tableName: 'signalvalue'
+  });
+};
