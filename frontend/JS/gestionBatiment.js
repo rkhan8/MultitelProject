3f20a34d6db49b260c9cf5725566d0342ef9ac75
lilperso <kouame-ange-martial.konan.1@ulaval.ca>
2017-04-23 01:17:06 -0400

    correction bug recherche batiment

--- frontend/JS/gestionBatiment.js
+++ frontend/JS/gestionBatiment.js
@@ -1,39 +1,30 @@
-function populateComboboxDateFromArray(comboboxId, array){
-  ///ANGE
+function populateComboboxDateFromArray(comboboxId, array) {
+    ///ANGE
 }
 
 
-function SearchBatimentGenerator(){
 
-  var compagnie = $('#CompagnieListbox option:selected').text();
-  var batiment = $('#BatListbox option:selected').text();
-  var adresse = $('#AdressListbox option:selected').text();
-
-
-  socket.emit('searchBatimentValues', compagnie, batiment, adresse);
-}
 
 
 function populateBatimentValuesTable(batimentValues) {
 
-  $("#tableBatiment tr").remove(); //remove previous search data
+    $("#tableBatiment tr").remove(); //remove previous search data
     var table = document.getElementById("tableBatiment");
-  //show element first table (id, cat, oldVolume, newVolume)
-  for(var j =0; j < batimentValues.length; j++)
-  {
-    //inserting table content by incrementing all the element into the arraylist
+    //show element first table (id, cat, oldVolume, newVolume)
+    for (var j = 0; j < batimentValues.length; j++) {
+        //inserting table content by incrementing all the element into the arraylist
 
-    var row = table.insertRow(table.rows.length);
+        var row = table.insertRow(table.rows.length);
 
-    //A MODIFIER
-    row.insertCell(0).innerHTML = batimentValues[j].Compagnie;
-    row.insertCell(1).innerHTML = batimentValues[j].NomBatiment;
-    row.insertCell(2).innerHTML = batimentValues[j].signal.MinValue;
-    row.insertCell(3).innerHTML = batimentValues[j].Adressse;
-    row.insertCell(4).innerHTML = batimentValues[j].Etage;
-    row.insertCell(5).innerHTML = batimentValues[j].NbCapteur;
+        //A MODIFIER
+        row.insertCell(0).innerHTML = batimentValues[j].Compagnie;
+        row.insertCell(1).innerHTML = batimentValues[j].NomBatiment;
+        row.insertCell(2).innerHTML = batimentValues[j].signal.MinValue;
+        row.insertCell(3).innerHTML = batimentValues[j].Adressse;
+        row.insertCell(4).innerHTML = batimentValues[j].Etage;
+        row.insertCell(5).innerHTML = batimentValues[j].NbCapteur;
 
-  }
+    }
 
 }
 
@@ -41,23 +32,33 @@ var socket = io();
 
 socket.emit('getComapgniesName');
 
-socket.on('batimentAjouterOk',function(){
-  PopUpCreer();
+socket.on('batimentAjouterOk', function () {
+    PopUpCreer();
 });
 
-socket.on('compagnie', function(data){
-    populateComboboxFromArray('compagnieList',data)
+socket.on('compagnie', function (data) {
+    populateComboboxFromArray('CompagnieListbox', data)
 });
 
-socket.on('batimentsName', function(data){
-    populateComboboxFromArray('batimentListbox',data)
+socket.on('batimentsName', function (data) {
+    $('#batimentListbox option').remove();
+    populateComboboxFromArray('batimentListbox', data)
 });
 
-$(function(){
-    $('#compagnieList').change(function() {
+$(function () {
+    $('#CompagnieListbox').change(function () {
         socket.emit('getCompagnieBatiment', $(this).find('option:selected').val());
     });
 });
+
+function SearchBatimentGenerator() {
+
+    socket.emit('searchBatimentValues', {
+        compagnie: $('#CompagnieListbox option:selected').text(),
+        batiment: $('#batimentListbox option:selected').text(),
+        adresse: $('#AdressListbox option:selected').text(),
+    });
+}
 
 function CreateBatiment() {
 
@@ -71,18 +72,18 @@ function CreateBatiment() {
     });
 }
 
-function PopUpCreer(){
-  var dialog = document.querySelector('#SuccessPopUp');
-  dialog.showModal();
-  dialog.querySelector('.close').addEventListener('click', function() {
-    dialog.close();
-  });
+function PopUpCreer() {
+    var dialog = document.querySelector('#SuccessPopUp');
+    dialog.showModal();
+    dialog.querySelector('.close').addEventListener('click', function () {
+        dialog.close();
+    });
 }
 
 
 function populateComboboxFromArray(comboboxId, array) {
     var data = '<option></option>'
-    $('#' + comboboxId).append(data);
+       $('#' + comboboxId).append(data);
 
     for (i = 0; i < array.length; i++) {
         var data = '<option>' + array[i] + '</option>'
@@ -91,11 +92,11 @@ function populateComboboxFromArray(comboboxId, array) {
 }
 
 
-function ValidateField(){
-  debugger;
-  if($("#compagnie").val() == "" || $("#batiment").val() == ""){
-    $("#addBatimentButton").attr('disabled', true);
-  } else if($("#compagnie").val() != "" && $("#batiment").val() != ""){
-    $("#addBatimentButton").attr('disabled', false);
-  }
+function ValidateField() {
+    debugger;
+    if ($("#compagnie").val() == "" || $("#batiment").val() == "") {
+        $("#addBatimentButton").attr('disabled', true);
+    } else if ($("#compagnie").val() != "" && $("#batiment").val() != "") {
+        $("#addBatimentButton").attr('disabled', false);
+    }
 }
