0c0fa5ee37b59359c62bf57cd101aed3df6d9ed4
TheBlurOSX <TheBlurOSX@TheBlurOSXs-MacBook-Air.local>
2017-04-22 20:02:04 -0400

    get position

--- frontend/JS/gestionGenerateur.js
+++ frontend/JS/gestionGenerateur.js
@@ -60,7 +60,7 @@ $(function () {
 
             if ($(generator).hasClass('ui-draggable')) {
                 $(generator).addClass('drag');
-                //$(generator).removeClass('ui-draggable');
+                $(generator).removeClass('ui-draggable');
             }
 
             if ($(generator).hasClass('generator')) {
